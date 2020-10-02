383556c444c22bf8efe4892929654904ae090700
Bezhnar Sergey <unkprog@gmail.com>
2020-10-02 14:31:09 +0300

    Рефакторинг

--- AutoPartsSite/AutoPartsSite/Startup.cs
+++ AutoPartsSite/AutoPartsSite/Startup.cs
@@ -1,19 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AutoPartsSite
 {
