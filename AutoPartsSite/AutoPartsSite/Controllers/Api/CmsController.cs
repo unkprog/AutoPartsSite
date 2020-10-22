47be5d53c7c95fe4386c30a6e4fb0f448d4adc67
Bezhnar Sergey <unkprog@gmail.com>
2020-10-22 10:25:41 +0300

    Рефакторинг

--- AutoPartsSite/AutoPartsSite/Controllers/Api/CmsController.cs
+++ AutoPartsSite/AutoPartsSite/Controllers/Api/CmsController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using AutoPartsSite.Core.Controllers;
 using AutoPartsSite.Core.Http;
