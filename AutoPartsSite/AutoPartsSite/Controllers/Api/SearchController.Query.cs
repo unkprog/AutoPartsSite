69f56c09d2e068a29736ab46c5f9f20163638dd8
Bezhnar Sergey <unkprog@gmail.com>
2020-11-14 18:18:11 +0300

    Рефакторинг

--- AutoPartsSite/AutoPartsSite/Controllers/Api/SearchController.Query.cs
+++ AutoPartsSite/AutoPartsSite/Controllers/Api/SearchController.Query.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Linq;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using AutoPartsSite.Core.Sql;
@@ -107,7 +108,7 @@ namespace AutoPartsSite.Controllers.Api
         [NonAction]
         private List<Goods> GetGoods(List<GoodsSearch> goods, PartNumberQuery pq, bool isGuest)
         {
-            List<Goods> result = new List<Goods>();
+           
             string partsXML = BuildPartsXML(goods);
 
             int f_Id = -1, f_Articul = -1, f_PartNumber = -1, f_Name = -1, f_Price = -1, f_ShipInDays = -1;
@@ -115,6 +116,8 @@ namespace AutoPartsSite.Controllers.Api
             int f_CountryId = -1, f_CountryCode = -1, f_CountryName = -1;
             int f_CurrencyId = -1, f_CurrencyCode = -1, f_CurrencyName = -1, f_CurrencySymbol = -1;
             int f_WeightPhysical = -1, f_WeightVolumetric = -1, f_LengthCm = -1, f_WidthCm = -1, f_HeightCm = -1;
+
+            Dictionary<int, Goods> result = new Dictionary<int, Goods>();
 
             ExecQuery((query) =>
             {
@@ -160,37 +163,43 @@ namespace AutoPartsSite.Controllers.Api
                 }
                 , (values) =>
                 {
-                    Goods item = new Goods() { Brand = new Brand(), Country = new Country(), Currency = new Currency(), Parameters = new GoodsParameters() };
-                    if (f_Id         > -1) item.Id         = values[f_Id].ToInt();
-                    if (f_PartNumber > -1) item.PartNumber = values[f_PartNumber].ToStr();
-                    if (f_Articul    > -1) item.Articul    = values[f_Articul].ToStr();
-                    if (f_Name       > -1) item.Name       = values[f_Name].ToStr();
-                    if (f_Price      > -1) item.Price      = values[f_Price].ToDecimal();
-                    if (f_ShipInDays > -1) item.ShipInDays = values[f_ShipInDays].ToInt();
+                    int id = 0;
+                    if (f_Id > -1) id = values[f_Id].ToInt();
+                    if(id > 0)
+                    {
+                        Goods item = null;
+                        if (!result.TryGetValue(id, out item))
+                        {
+                            item = new Goods() { Id = id, Brand = new Brand(), Country = new Country(), Currency = new Currency(), Parameters = new GoodsParameters() };
+                            result.Add(id, item);
+                        }
+                        if (f_PartNumber > -1) item.PartNumber = values[f_PartNumber].ToStr();
+                        if (f_Articul    > -1) item.Articul    = values[f_Articul].ToStr();
+                        if (f_Name       > -1) item.Name       = values[f_Name].ToStr();
+                        if (f_Price      > -1) item.Price      = values[f_Price].ToDecimal();
+                        if (f_ShipInDays > -1) item.ShipInDays = values[f_ShipInDays].ToInt();
 
-                    if (f_BrandId   > -1) item.Brand.Id   = values[f_BrandId].ToInt();
-                    if (f_BrandCode > -1) item.Brand.Code = values[f_BrandCode].ToStr();
+                        if (f_BrandId   > -1) item.Brand.Id   = values[f_BrandId].ToInt();
+                        if (f_BrandCode > -1) item.Brand.Code = values[f_BrandCode].ToStr();
 
-                    if (f_CountryId   > -1) item.Country.Id   = values[f_CountryId].ToInt();
-                    if (f_CountryCode > -1) item.Country.Code = values[f_CountryCode].ToStr();
-                    if (f_CountryName > -1) item.Country.Name = values[f_CountryName].ToStr();
+                        if (f_CountryId   > -1) item.Country.Id   = values[f_CountryId].ToInt();
+                        if (f_CountryCode > -1) item.Country.Code = values[f_CountryCode].ToStr();
+                        if (f_CountryName > -1) item.Country.Name = values[f_CountryName].ToStr();
 
-                    if (f_CurrencyId     > -1) item.Currency.Id     = values[f_CurrencyId].ToInt();
-                    if (f_CurrencyCode   > -1) item.Currency.Code   = values[f_CurrencyCode].ToStr();
-                    if (f_CurrencyName   > -1) item.Currency.Name   = values[f_CurrencyName].ToStr();
-                    if (f_CurrencySymbol > -1) item.Currency.Symbol = values[f_CurrencySymbol].ToStr();
+                        if (f_CurrencyId     > -1) item.Currency.Id     = values[f_CurrencyId].ToInt();
+                        if (f_CurrencyCode   > -1) item.Currency.Code   = values[f_CurrencyCode].ToStr();
+                        if (f_CurrencyName   > -1) item.Currency.Name   = values[f_CurrencyName].ToStr();
+                        if (f_CurrencySymbol > -1) item.Currency.Symbol = values[f_CurrencySymbol].ToStr();
 
-                    if (f_WeightPhysical   > -1) item.Parameters.WeightPhysical   = values[f_WeightPhysical].ToDecimal();
-                    if (f_WeightVolumetric > -1) item.Parameters.WeightVolumetric = values[f_WeightVolumetric].ToDecimal();
-                    if (f_LengthCm         > -1) item.Parameters.LengthCm         = values[f_LengthCm].ToDecimal();
-                    if (f_WidthCm          > -1) item.Parameters.WidthCm          = values[f_WidthCm].ToDecimal();
-                    if (f_HeightCm         > -1) item.Parameters.HeightCm         = values[f_HeightCm].ToDecimal();
-
-
-                    result.Add(item);
+                        if (f_WeightPhysical   > -1) item.Parameters.WeightPhysical   = values[f_WeightPhysical].ToDecimal();
+                        if (f_WeightVolumetric > -1) item.Parameters.WeightVolumetric = values[f_WeightVolumetric].ToDecimal();
+                        if (f_LengthCm         > -1) item.Parameters.LengthCm         = values[f_LengthCm].ToDecimal();
+                        if (f_WidthCm          > -1) item.Parameters.WidthCm          = values[f_WidthCm].ToDecimal();
+                        if (f_HeightCm         > -1) item.Parameters.HeightCm         = values[f_HeightCm].ToDecimal();
+                    }
                 });
             });
-            return result;
+            return result.Values.ToList();
         }
 
         //[NonAction]
