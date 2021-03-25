84d47cf3d02258349b31c17ec321d1943c9f1491
unkprog <unkprog@gmail.com>
2021-03-25 23:31:30 +0300

    Добавление проекта для экспорта данных

---
+++ AutoPartsSite/AutoPartsSite.Util.Exporter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoPartsSite.Util.Exporter.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ AutoPartsSite/AutoPartsSite.Util.Exporter/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoPartsSite.Util.Exporter.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
